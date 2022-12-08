4adc4707d2bb36d097702877f6134fd69b997634
Silink2309 <msmirnov016@gmail.com>
2022-12-08 23:42:15 +0300

    Правки4

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -22,49 +22,48 @@ public class HomePageTest {
 
     }
     //
-    @Test
-    public void orderDown(){
 
-       HomePage objHomePage = new HomePage(driver);
-       objHomePage.clickOrderButtonDown();
-    }
-    @Test
-    public void cookieTest(){
-        HomePage objHomePage = new HomePage(driver);
-        objHomePage.clickCookieButton();
-    }
     @Test
     public void accordionTest0(){
+
         HomePage objHomePage = new HomePage(driver);
+        objHomePage.clickCookieButton();//Без нкк
         objHomePage.testAccordion0();
     }
     @Test
     public void accordionTest1(){
         HomePage objHomePage = new HomePage(driver);
+        objHomePage.clickCookieButton();
         objHomePage.testAccordion1();}
     @Test
     public void accordionTest2(){
         HomePage objHomePage = new HomePage(driver);
+        objHomePage.clickCookieButton();
         objHomePage.testAccordion2();}
     @Test
     public void accordionTest3(){
         HomePage objHomePage = new HomePage(driver);
+        objHomePage.clickCookieButton();
         objHomePage.testAccordion3();}
     @Test
     public void accordionTest4(){
         HomePage objHomePage = new HomePage(driver);
+        objHomePage.clickCookieButton();
         objHomePage.testAccordion4();}
     @Test
     public void accordionTest5(){
         HomePage objHomePage = new HomePage(driver);
+        objHomePage.clickCookieButton();
         objHomePage.testAccordion5();}
     @Test
     public void accordionTest6(){
         HomePage objHomePage = new HomePage(driver);
+        objHomePage.clickCookieButton();
         objHomePage.testAccordion6();}
     @Test
     public void accordionTest7(){
         HomePage objHomePage = new HomePage(driver);
+        objHomePage.clickCookieButton();
         objHomePage.testAccordion7();}
 
     @After
